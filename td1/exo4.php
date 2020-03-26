6359a180251a3a1b394533b8eec6ed042a4799cd
MaevaMACE <62309146+MaevaMACE@users.noreply.github.com>
2020-03-26 16:58:36 +0100

    quatrième commit

--- td1/exo4.php
+++ td1/exo4.php
@@ -4,30 +4,25 @@
     <title>ex4</title>
 </head>
 <body>
-    <a href="?nblignes=2&nbColonnes=2">2x2</a>
-    <a href="?nblignes=5&nbColonnes=5">5x5</a>
-    <a href="?nblignes=10&nbColonnes=8">10x8</a>
-
+    <a href="?ligne=2&Colonne=2">2x2</a>
+    <a href="?ligne=5&Colonne=5">5x5</a>
+    <a href="?ligne=10&Colonne=8">10x8</a>
 
     <?php
     echo "<table border='1'>
     <tbody>";
-    for($l=0;$l<=$_GET["nblignes"];$l++){
-
-        if (($l % 2) == 0){
+    for($l=0;$l<=$_GET["ligne"];$l++){
+       if (($l % 2) == 0){
             $gras = "bold";
         }
-        echo "<tr>"; 
-        for($c=0;$c<=$_GET["nbColonnes"];$c++){ 
-            echo "<td align='center'><p style='font-weight: bold;'>$l-$c</p></td>"; 
+       echo "<tr>"; 
+    for($c=0;$c<=$_GET["Colonne"];$c++){ 
+        echo "<td align='center'><p style='font-weight: bold;'>$l-$c</p></td>"; 
         } 
-        echo "</tr>"; 
+       echo "</tr>"; 
     } 
-
-    echo "</tbody>
-    </table>";
-
+		echo "</tbody>
+			</table>";
     ?>
-
 </body>
 </html>
