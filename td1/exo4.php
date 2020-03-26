7f5f1f7342d78b4bb76b1182491d83b77a094294
MaevaMACE <62309146+MaevaMACE@users.noreply.github.com>
2020-03-26 17:08:40 +0100

    cinq commit

--- td1/exo4.php
+++ td1/exo4.php
@@ -7,22 +7,34 @@
     <a href="?ligne=2&Colonne=2">2x2</a>
     <a href="?ligne=5&Colonne=5">5x5</a>
     <a href="?ligne=10&Colonne=8">10x8</a>
-
     <?php
     echo "<table border='1'>
     <tbody>";
     for($l=0;$l<=$_GET["ligne"];$l++){
-       if (($l % 2) == 0){
+
+        if (($l % 2) == 0){
             $gras = "bold";
         }
-       echo "<tr>"; 
-    for($c=0;$c<=$_GET["Colonne"];$c++){ 
-        echo "<td align='center'><p style='font-weight: bold;'>$l-$c</p></td>"; 
+        else{
+            $gras = "normal";
+        }
+        echo "<tr>"; 
+        for($c=0;$c<=$_GET["Colonne"];$c++){ 
+            if (($c % 2) == 1){
+                $color = "red";
+            }
+            else{
+                $color = "black";
+            }
+            echo "<td align='center'><p style='font-weight: $gras; color: $color'>$l-$c</p></td>"; 
         } 
-       echo "</tr>"; 
+        echo "</tr>"; 
     } 
-		echo "</tbody>
-			</table>";
+
+    echo "</tbody>
+    </table>";
+
     ?>
+
 </body>
 </html>
