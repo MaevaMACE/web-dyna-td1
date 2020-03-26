f20762e59f93a14aa137255814586f4107f96c1c
MaevaMACE <62309146+MaevaMACE@users.noreply.github.com>
2020-03-26 13:55:12 +0100

    premier commit

---
+++ td1/exo1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hello</title>
+</head>
+<body>
+<a href="/tpsmacé/tp1/exo1.php?message=Hello">Afficher Hello</a>
+<?php
+echo "Vous êtes sur la page n° ";
+echo "Je m'appelle Maeva";
+echo "Je suis une fille";
+?>
+</body>
+</html>
