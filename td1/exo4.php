2f4e2ee00513f39035f8776b745152c335d37980
MaevaMACE <62309146+MaevaMACE@users.noreply.github.com>
2020-03-26 16:46:10 +0100

    troisieme commit

--- td1/exo4.php
+++ td1/exo4.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Hello</title>
+    <title>ex4</title>
 </head>
-
 <body>
-	<table border='1'>
-		<tbody>
-			<tr>
-				<td>1</td>
-				<td>2</td>
-			</tr>
-			<tr>
-				<td>3</td>
-				<td>4</td>
-			</tr>
-		</tbody>
-	</table>
-	<a href="?nbligne=2&nbcolonne=2
+    <a href="?nblignes=2&nbColonnes=2">2x2</a>
+    <a href="?nblignes=5&nbColonnes=5">5x5</a>
+    <a href="?nblignes=10&nbColonnes=8">10x8</a>
+
+
+    <?php
+    echo "<table border='1'>
+    <tbody>";
+    for($l=0;$l<=$_GET["nblignes"];$l++){
+
+        if (($l % 2) == 0){
+            $gras = "bold";
+        }
+        echo "<tr>"; 
+        for($c=0;$c<=$_GET["nbColonnes"];$c++){ 
+            echo "<td align='center'><p style='font-weight: bold;'>$l-$c</p></td>"; 
+        } 
+        echo "</tr>"; 
+    } 
+
+    echo "</tbody>
+    </table>";
+
+    ?>
+
 </body>
 </html>
