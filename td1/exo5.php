b573af83026b3fecc1e6c32e5c81c93ec41a049d
MaevaMACE <62309146+MaevaMACE@users.noreply.github.com>
2020-04-09 15:41:50 +0200

    commit tp2un

--- td1/exo5.php
+++ td1/exo5.php
@@ -6,15 +6,15 @@
 </head>
 <body>
 			<form method='POST'>
-				<label for="ligne">lignes</label>
+				<label for($l=1;$l<$nbLignes;$l++)>lignes</label>
 				<input id="nbLignes" name="nbLignes"></input>
-				<label for="contenue">colonnes</label>
+				<label for($c=1;$c<$nbColonnes;$c++){>colonnes</label>
 				<input id="nbColonnes" name="nbColonnes"></input>
 				<input type="submit" value="Valider">
 			</form>
 	<?php
 		include('function.php');
-		echo createHtmlTable($_POST['nbColonnes']-1,$_POST['nbLignes']-1);
+		echo createHtmlTable($_POST['nbLignes'],$_POST['nbColonnes']);
 	?>
 </body>
 </html>
