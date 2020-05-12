7486d642253ab38735d747a5ee56f10fe30d5aa8
MaevaMACE <62309146+MaevaMACE@users.noreply.github.com>
2020-05-12 15:22:13 +0200

    tpnoté6

--- tp4/app/controllers/Inscription.php
+++ tp4/app/controllers/Inscription.php
@@ -8,4 +8,12 @@ class Inscription extends ControllerBase{
 	public function index(){
 		$this->loadView("Inscription/index.html");
 	}
+
+	/**
+	 *@route("/do/inscription","methods"=>["post"])
+	**/
+	public function do(){
+		
+	}
+
 }
